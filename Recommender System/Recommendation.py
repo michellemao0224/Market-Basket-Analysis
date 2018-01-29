9be3ddfa90ad5919d127aa5cf4ed472c5d3e4909
U5877271@anu.edu.au <Miyal@Lanmixues-Air.lan>
2018-01-30 00:01:27 +1100

    edit recommender

--- Recommender System/Recommendation.py
+++ Recommender System/Recommendation.py
@@ -22,7 +22,7 @@ for name in itemTable.index:
     # print(itemTable.loc[name])
     # df['Product'] = itemTable.loc[name]
 df['Product'] = pd.Series(product)
-print(df['Product'])
+# print(df['Product'])
 
 #  Content Based Recommender
 #Import TfIdfVectorizer from scikit-learn
@@ -38,10 +38,37 @@ df['Product'] = df['Product'].fillna('')
 tfidf_matrix = tfidf.fit_transform(df['Product'])
 
 #Output the shape of tfidf_matrix
-print(tfidf_matrix.shape)
-# over 75,000 different words were used to describe the 45,000 movies in your dataset
+# print(tfidf_matrix.shape)
+# (531285, 2129)
+# over 531285 different words were used to describe the 2129 products in the dataset
 
+# Import linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
 
-# print(df[df['CustomerID']== 17850])
+# Compute the cosine similarity matrix
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-descrptions = df[df['CustomerID']== 17850]['Description']
+#Construct a reverse map of indices and product descriptions
+indices = pd.Series(df.index, index=df['Product']).drop_duplicates()
+
+# Function that takes in product description as input and outputs most similar products
+def get_recommendations(product, cosine_sim=cosine_sim):
+    # Get the index of the product that matches the description
+    idx = indices[product]
+
+    # Get the pairwsie similarity scores of all products with that product
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the products based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar products
+    sim_scores = sim_scores[1:11]
+
+    # Get the product indices
+    product_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar products
+    return df['Product'].iloc[product_indices]
+
+print(get_recommendations('ALARM CLOCK BAKELIKE PINK'))
